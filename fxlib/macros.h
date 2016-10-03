e9594ed456e63fd03f563a0dd5b0ad0bdbb8440d
Jiří Janoušek <janousek.jiri@gmail.com>
2016-10-03 22:14:01 +0200

    Add FX_DEBUG macro
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- fxlib/macros.h
+++ fxlib/macros.h
@@ -12,14 +12,22 @@
 
 #ifndef FXLIB_PEBBLE
 #include <stdio.h>  // fprintf, fputs
+#define fx_debug(filename, line, format, ...)                                                     \
+	fprintf(stderr, "*** Debug *** %s:%d: ", filename, line);                                     \
+	fprintf(stderr, format, ##__VA_ARGS__);                                                       \
+	fputs(stderr, "\n");
 #define fx_log_err(filename, line, format, ...)                                                   \
 	fprintf(stderr, "*** Error *** %s:%d: ", filename, line);                                     \
 	fprintf(stderr, format, ##__VA_ARGS__);                                                       \
 	fputs(stderr, "\n");
 #else
+#define fx_debug(filename, line, format, ...)                                                     \
+  app_log(APP_LOG_LEVEL_DEBUG, filename, line, format, ##__VA_ARGS__)
 #define fx_log_err(filename, line, format, ...)                                                   \
   app_log(APP_LOG_LEVEL_ERROR, filename, line, format, ##__VA_ARGS__)
 #endif
+
+#define FX_DEBUG(format, ...)   fx_debug(__FILE__, __LINE__, format, ##__VA_ARGS__)  
 
 #define fx_malloc(type) ((type*) malloc(sizeof(type)))
 #define fx_alloc(n, type) ((type*) malloc((n) * sizeof(type)))
